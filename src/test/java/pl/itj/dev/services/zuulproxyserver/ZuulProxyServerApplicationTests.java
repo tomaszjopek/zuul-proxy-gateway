5a9afaf7c7d8e3877c68765480d84833b238f53c
Tomasz <tom.jopek@gmail.com>
2020-01-06 22:13:48 +0100

    'init'

---
+++ src/test/java/pl/itj/dev/services/zuulproxyserver/ZuulProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.itj.dev.services.zuulproxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
